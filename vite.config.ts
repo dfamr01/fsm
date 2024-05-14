53e35ecad3e044db60b8bf452e559aefd05a2917
daivd_abu <david@livly.com>
2024-05-14 14:23:36 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/intuit/",
+  base: "/Intuit/",
   build: {
     outDir: "dist", // Confirm this line or adjust according to your setup
   },
