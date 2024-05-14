7ce6755d95c5de1523da5e79062bfd607baa3b77
daivd_abu <david@livly.com>
2024-05-14 14:39:47 +0300

    temp

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/Intuit/",
+  build: {
+    outDir: "dist", // Confirm this line or adjust according to your setup
+  },
+});
